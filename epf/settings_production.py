84d6e2d6b8c390eac0d95b3bdf8b0d50d82d0bb5
Anders Pearson <anders@columbia.edu>
2010-01-12 16:41:41 -0700

    importing code

---
+++ epf/settings_production.py
@@ -0,0 +1,10 @@
+from settings_shared import *
+
+TEMPLATE_DIRS = (
+    "/var/www/epf/epf/templates",
+)
+
+MEDIA_ROOT = '/var/www/epf/uploads/'
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
---
+++ settings_production.py
@@ -0,0 +1,10 @@
+from settings_shared import *
+
+TEMPLATE_DIRS = (
+    "/var/www/npo/npo/templates",
+)
+
+MEDIA_ROOT = '/var/www/npo/uploads/'
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
